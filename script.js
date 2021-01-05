37c4047cc91233a4d87cddc3fdf81efb9da4d40e
GeekSilver <erickioko995@gmail.com>
2021-01-05 23:51:07 +0300

    feat: smooth scroll
    added a jquery smooth scroll function

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ const toggleSocialBtns = function () {
     }
     
 }
+
+$("#navbarPagesNav ul a").on("click", function (e) {
+    // 1
+    e.preventDefault();
+    // 2
+    const href = $(this).attr("href");
+    // 3
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+  });
