5b9e05c0f0badda003e6d3460245c165c294ebd7
GeekSilver <erickioko995@gmail.com>
2021-04-05 19:28:11 +0300

    FIX: added smooth scroll to all nav links and make an inquiry buttons

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-
-
 const toggleSocialBtns = function () {
-    // blur lead text
-    const leadH1 = document.querySelector('#lead-h1');
-    const leadText = document.querySelector('#lead-text');
-    leadH1.classList.toggle('blurLeadText');
-    leadText.classList.toggle('blurLeadText');
-    // hide & show social icons
-    const toggler = document.querySelectorAll('#hiddenOnMobile');
-    for (let index = 0; index < toggler.length; index++) {
-        const element = toggler[index];
-        element.classList.toggle('hideSpan'); 
-    }
-    
-}
+  // blur lead text
+  const leadH1 = document.querySelector("#lead-h1");
+  const leadText = document.querySelector("#lead-text");
+  leadH1.classList.toggle("blurLeadText");
+  leadText.classList.toggle("blurLeadText");
+  // hide & show social icons
+  const toggler = document.querySelectorAll("#hiddenOnMobile");
+  for (let index = 0; index < toggler.length; index++) {
+    const element = toggler[index];
+    element.classList.toggle("hideSpan");
+  }
+};
 
-$("#navbarPagesNav ul a").on("click", function (e) {
-    // 1
-    e.preventDefault();
-    // 2
-    const href = $(this).attr("href");
-    // 3
-    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
-  });
+// nav-links smooth scroll
+$(".nav-link").on("click", function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr("href");
+  // 3
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+
+// smooth scroll onclick make inquiry button
+$(".btn-make-inquiry").on("click", function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr("href");
+  // 3
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
